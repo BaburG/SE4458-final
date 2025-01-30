20564fd930dda004df69ff74ed504f6a294c700a
BaburG <babur.g@hotmail.com>
2025-01-30 14:50:07 +0300

    api gateway added

--- doctor_frontend/vite.config.js
+++ doctor_frontend/vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/doctor/',
   server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
     proxy: {
       '/api': {
         target: 'http://medicine_service:8000',
--- pharmacy_frontend/vite.config.ts
+++ pharmacy_frontend/vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pharmacy/',
   server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
     proxy: {
       '/prescription': {
         target: 'http://prescription_service:8000',
